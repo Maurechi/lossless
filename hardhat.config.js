b1ed7530f28236a5546a3482de26da5dcb695125
mauro guz <mauroguzbejar@gmail.com>
2021-11-10 11:42:29 -0300

    hardhat config for deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,21 +3,6 @@ require('dotenv').config()
 require('@typechain/hardhat')
 require('@nomiclabs/hardhat-ethers')
 require('@nomiclabs/hardhat-waffle')
-const { task } = require('hardhat/config')
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async (args, hre) => {
-  const accounts = await hre.ethers.getSigners()
-
-  for (const account of accounts) {
-    console.log(account.address)
-  }
-})
-// eslint-disable-next-line no-restricted-syntax
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -37,20 +22,5 @@ module.exports = {
     hardhat: {
       chainId: 1337,
     },
-    ropsten: {
-      url: process.env.ROPSTEN_PROVIDER_URL,
-      accounts: {
-        mnemonic: process.env.MNEMONIC,
-      },
-    },
-    rinkeby: {
-      url: process.env.RINKEBY_PROVIDER_URL,
-      accounts: {
-        mnemonic: process.env.MNEMONIC,
-      },
-    },
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API,
   },
 }
